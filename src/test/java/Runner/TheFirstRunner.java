850c572bd827dc359f75e82f08141665ab1434d9
tsolmon <tsolmon_mm@yahoo.com>
2019-07-23 18:35:06 -0400

    added json plugin

--- src/test/java/Runner/TheFirstRunner.java
+++ src/test/java/Runner/TheFirstRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-        plugin={"html:target/Reports","rerun:target/Failed/rerun.txt"},
+        plugin={"json:FirstRunner.json","html:target/Reports","rerun:target/Failed/rerun.txt"},
         features ={"/Users/Tsolmon/IdeaProjects/vytrackCukesautomation/src/main/resources"}
         //features ={"/Users/Tsolmon/IdeaProjects/vytrackCukesautomation/src/main/resources"}
         ,glue = {"Step_definition"}
